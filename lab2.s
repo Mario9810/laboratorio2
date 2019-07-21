aa3fc51c15cc7f791f65a654734fc3aa4bc9a32d
Mario9810 <32363631+Mario9810@users.noreply.github.com>
2019-07-21 17:49:24 -0600

    commit
    ...

---
+++ lab2.s
@@ -0,0 +1,22 @@
+.global main 
+.func main 
+
+main:
+	LDR R8,=vector1	
+	LDR R9,=vector2
+	LDR R6,=vector3
+	MOV R10,#3
+//carga valor de vectores a los registros		
+asignacion:
+	VLDR S16, [R8]			
+	VLDR S18, [R8,#4]		
+	VLDR S20, [R8,#8]
+	VLDR S22, [R8,#12]
+    VLDR S22, [R8,#16]
+    
+
+
+.data
+vector1:	.float 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0,13.0,14.0,15.0,16.0
+vector2:	.float 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0,13.0,14.0,15.0,16.0
+vector3:	.float 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0,0.0,0.0
